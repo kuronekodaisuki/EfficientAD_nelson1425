84316a4b6f4aac61d618947d0c1fb440c22557e9
Shuji URANO <shuji.urano.fg@gmail.com>
2024-10-09 15:02:41 +0900

    Fix LoadParameters

--- Inference.py
+++ Inference.py
@@ -30,20 +30,24 @@ def main(config):
     teacher = torch.load(os.path.join(train_dir, 'teacher_final.pth'))
     student = torch.load(os.path.join(train_dir, 'student_final.pth'))
     autoencoder = torch.load(os.path.join(train_dir, 'autoencoder_final.pth'))
-    t_mean, t_std, g_std_s, g_std_e, g_ae_s, g_ae_e = LoadParameters(os.path.join(train_dir, 'parameters.yaml'))
+    t_mean, t_std, g_std_s, g_std_e, g_ae_s, g_ae_e = LoadParameters(train_dir)
 
 # Load parameters for Inference
 def LoadParameters(file_path):
-    with open(file_path, encoding='utf-8') as f:
-        parameters = yaml.safe_load(f)
-    return parameters['teacher_mean'], parameters['teacher_std'], parameters['g_st_start'], parameters['g_st_end'], parameters['g_ae_start'], parameters['g_ae_end']
+    teacher_mean = torch.load(os.path.join(file_path, 'teacher_mean.pt'))
+    teacher_std = torch.load(os.path.join(file_path, 'teacher_std.pt'))
+    q_st_start = torch.load(os.path.join(file_path, 'q_st_start.pt'))    
+    q_st_end = torch.load(os.path.join(file_path, 'q_st_end.pt'))    
+    q_ae_start = torch.load(os.path.join(file_path, 'q_ae_start.pt'))    
+    q_ae_end = torch.load(os.path.join(file_path, 'q_ae_end.pt'))    
+    return teacher_mean, teacher_std, q_st_start, q_st_end, q_ae_start, q_ae_end
         
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dataset', default='mvtec_loco',
                         choices=['mvtec_ad', 'mvtec_loco'])
-    parser.add_argument('-s', '--subdataset', default='bottle',
+    parser.add_argument('-s', '--subdataset', default='breakfast_box',
                         help='One of 15 sub-datasets of Mvtec AD or 5' +
                              'sub-datasets of Mvtec LOCO')
     parser.add_argument('-o', '--output_dir', default='output/1')
